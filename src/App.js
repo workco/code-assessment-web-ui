f7e3d2059ddcf4e3b48f6463adaf0ab5db878060
Sarah Mogin <mogin@work.co>
2019-11-11 15:02:13 -0500

    chore: set up React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import ProductLanding from './pages/ProductLanding';
+import ProductDetail from './pages/ProductDetail';
 import './App.scss';
 
 function App() {
   return (
-    <div>
-      Create React App
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <ProductLanding />
+        </Route>
+        <Route exact path="/product/:id">
+          <ProductDetail />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/ProductLanding/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductLanding() {
+  return (
+    <div>
+      ProductLanding
+    </div>
+  );
+}
+
+export default ProductLanding;
