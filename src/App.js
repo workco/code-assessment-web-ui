c975f4ec36fc1951f7d75339aae0c62c2d865b5a
Sarah Mogin <mogin@work.co>
2019-11-13 11:15:42 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ProductLanding from './pages/ProductLanding';
 import ProductDetail from './pages/ProductDetail';
 import './App.scss';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <ProductLanding />
-        </Route>
-        <Route exact path="/product/:id">
-          <ProductDetail />
-        </Route>
-      </Switch>
-    </Router>
-  );
+class App extends Component {
+  static initialState = {
+    products: [],
+    cartItems: [],
+    addToCart: this.onAddToCart
+  };
+
+  onAddToCart = () => {
+    console.log('Add to cart');
+  };
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <ProductLanding />
+          </Route>
+          <Route exact path="/product/:id">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
