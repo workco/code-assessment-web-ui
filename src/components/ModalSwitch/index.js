46ad16e30244eb6b2b2f3e585b64a59143016329
Sarah Mogin <mogin@work.co>
2019-11-13 17:48:48 -0500

    feat(Cart): implement Cart page as modal (#31)

---
+++ src/components/ModalSwitch/index.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useContext } from 'react';
+import { Link, Switch, Route, Redirect, useLocation } from 'react-router-dom';
+
+import AppContext from '../../contexts/app/context';
+
+import ProductLanding from '../../pages/ProductLanding';
+import Cart from '../../pages/Cart';
+
+function ModalSwitch() {
+  const { cartItems } = useContext(AppContext);
+  const location = useLocation();
+  const background = location.state && location.state.background;
+
+  const cartQuantity = cartItems.reduce((acc, item) => acc + item.count, 0);
+
+  return (
+    <Fragment>
+      {/* Set current page to be background of cart modal */}
+      <Link to={{ pathname: 'cart', state: { background: location } }}>
+        Cart - {cartQuantity}
+      </Link>
+      <Switch location={background || location}>
+        <Route exact path="/">
+          <ProductLanding />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+      {background && (
+        <Switch location={location}>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+        </Switch>
+      )}
+    </Fragment>
+  );
+}
+
+export default ModalSwitch;
---
+++ src/pages/Cart/index.js
@@ -0,0 +1,56 @@
+import React, { useContext } from 'react';
+import { css } from 'emotion';
+import { useHistory } from 'react-router-dom';
+
+import AppContext from '../../contexts/app/context';
+
+// These are temporary styles
+const styles = {
+  wrapper: css({
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.35)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }),
+  inner: css({
+    backgroundColor: 'white',
+    width: '80%',
+    height: '80%'
+  })
+};
+
+function Cart() {
+  const { cartItems, incrementItem, decrementItem } = useContext(AppContext);
+  const history = useHistory();
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.inner}>
+        <button onClick={() => history.goBack()}>Close</button>
+
+        <h1>Cart</h1>
+
+        <ul>
+          {cartItems.map(cartItem => {
+            const { id, title, count } = cartItem;
+            return (
+              <li key={id}>
+                <h2>{title}</h2>
+                <span>{count}</span>
+                <button onClick={() => incrementItem(cartItem)}>+</button>
+                <button onClick={() => decrementItem(cartItem)}>-</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
