012fc26e72fb6b8bac1416ad2ee0672e62d9b810
Sarah Mogin <mogin@work.co>
2019-12-09 14:59:04 -0500

    chore(Storybook): Add react router decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,10 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import StoryRouter from 'storybook-react-router';
 
 import '../src/styles/app.scss';
 
 addDecorator(withKnobs);
+addDecorator(StoryRouter());
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
