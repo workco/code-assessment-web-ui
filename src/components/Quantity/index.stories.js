f7c80a5817a74e253308d91895d4ada494e60143
Sarah Mogin <mogin@work.co>
2019-12-09 14:57:03 -0500

    chore(Storybook): Add actions addon

--- src/components/Quantity/index.stories.js
+++ src/components/Quantity/index.stories.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 
 import Quantity from './index';
 
 export default { title: 'Quantity' };
 
 export const regular = () => (
-  <Quantity onIncrement={() => {}} onDecrement={() => {}} count={0} />
+  <Quantity
+    onIncrement={action('on increment')}
+    onDecrement={action('on decrement')}
+    count={0}
+  />
 );
