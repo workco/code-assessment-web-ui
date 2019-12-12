b1bdd4b1b92bf115039b3b6380c3ce366efc6999
Sarah Mogin <mogin@work.co>
2019-12-12 18:12:05 -0500

    bug: Video gets smaller on tablet, doesnt get bigger again
    SPLASH SCREEN: Video player not resizing from tablet to desktop

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './Splash.module.scss';
 import { Link } from 'react-router-dom';
@@ -7,13 +7,29 @@ import BrowserDetection from 'react-browser-detection';
 import { getImageUrl } from '../../utils/images';
 
 function Splash() {
+  const [width, setWidth] = useState(1400);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (width >= 1100) {
+        setWidth(window.innerWidth);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [width]);
+
+  console.log(width);
+
   return (
     <main className={styles.wrapper}>
       <h1 className={styles.title}>
         <Link to="/">Daily deals</Link>
       </h1>
 
-      <BrowserDetection>
+      <BrowserDetection key={width >= 1100}>
         {{
           default: browser => (
             <video
@@ -21,6 +37,7 @@ function Splash() {
               className={styles.video}
               autoPlay
               muted
+              width={width >= 1100 ? '80%' : '50%'}
               loop={browser === 'chrome'} /* Only loop on Chrome */
             >
               <source src={getImageUrl('/assets/video.mp4')} type="video/mp4" />
