65c08351ec5d701f454bdc550a55aa00697c5b72
Sarah Mogin <mogin@work.co>
2019-12-18 11:36:06 -0500

    bug: Horizontal scroll on mobile chrome PLP
    STORE PAGE: Horizontal scroll on mobile view

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import BrowserDetection from 'react-browser-detection';
 import cx from 'classnames';
 
 import Button from '../Button';
@@ -20,37 +21,52 @@ const Product = ({
   price,
   title
 }) => {
-  const isInCart = onIncrement && onDecrement;
-  const productClasses = cx(className, styles.product, {
-    [styles.inProductLanding]: !isInCart,
-    [styles.inCart]: isInCart,
-    [styles.featured]: isFeatured
-  });
-
-  const imageSrc = isFeatured ? getImage(images, 'featured') : getImage(images);
-  const finalPrice = (price * count).toFixed(2);
-
   return (
-    <div className={productClasses}>
-      <img className={styles.image} src={imageSrc} alt={title} />
-      <div className={styles.details}>
-        <div className={styles.text}>
-          <h2 className={styles.title}>{title}</h2>
-          <span className={styles.price}>${finalPrice}</span>
-        </div>
-        {isInCart ? (
-          <Quantity
-            onIncrement={onIncrement}
-            onDecrement={onDecrement}
-            count={count}
-          />
-        ) : (
-          <Button className={styles.addButton} onClick={onClick}>
-            Add to bag
-          </Button>
-        )}
-      </div>
-    </div>
+    <BrowserDetection>
+      {{
+        default: browser => {
+          const isInCart = onIncrement && onDecrement;
+          const productClasses = cx(
+            className,
+            styles.product,
+            styles[`product-${browser}`],
+            {
+              [styles.inProductLanding]: !isInCart,
+              [styles.inCart]: isInCart,
+              [styles.featured]: isFeatured
+            }
+          );
+
+          const imageSrc = isFeatured
+            ? getImage(images, 'featured')
+            : getImage(images);
+          const finalPrice = (price * count).toFixed(2);
+
+          return (
+            <div className={productClasses}>
+              <img className={styles.image} src={imageSrc} alt={title} />
+              <div className={styles.details}>
+                <div className={styles.text}>
+                  <h2 className={styles.title}>{title}</h2>
+                  <span className={styles.price}>${finalPrice}</span>
+                </div>
+                {isInCart ? (
+                  <Quantity
+                    onIncrement={onIncrement}
+                    onDecrement={onDecrement}
+                    count={count}
+                  />
+                ) : (
+                  <Button className={styles.addButton} onClick={onClick}>
+                    Add to bag
+                  </Button>
+                )}
+              </div>
+            </div>
+          );
+        }
+      }}
+    </BrowserDetection>
   );
 };
 
