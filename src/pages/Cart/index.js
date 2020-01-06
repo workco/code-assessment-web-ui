369a1319c90ad2cea35bfa6d7ddf3a2bde2296da
Sarah Mogin <mogin@work.co>
2020-01-06 11:42:25 -0500

    bug: Modal does not close on bg click on Chrome
    CART MODAL : Clicking outside modal should close it

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import cx from 'classnames';
 import { Link, withRouter } from 'react-router-dom';
+import BrowserDetection from 'react-browser-detection';
 
 import AppContext from '../../contexts/AppContext';
 
@@ -26,87 +27,108 @@ function Cart({ history }) {
   }
 
   return (
-    <div
-      className={styles.wrapper}
-      onClick={e => {
-        if (e.target === e.currentTarget) {
-          history.push('/products');
-        }
-      }}
-    >
-      <div className={innerClasses}>
-        <Link to="/products" className={styles.closeBtn}>
-          <img src={close} alt="close" />
-        </Link>
+    <BrowserDetection once={false}>
+      {{
+        default: browser => (
+          <div
+            className={styles.wrapper}
+            onClick={e => {
+              if (e.target === e.currentTarget && browser !== 'chrome') {
+                history.push('/products');
+              }
+            }}
+          >
+            <div className={innerClasses}>
+              <Link to="/products" className={styles.closeBtn}>
+                <img src={close} alt="close" />
+              </Link>
 
-        {!!cartItems.length ? (
-          <>
-            <div className={cx(styles.products, styles.section)}>
-              <h2 className={styles.heading}>Shopping Bag</h2>
-              <ul className={styles.productList}>
-                {cartItems.map(cartItem => (
-                  <Product
-                    {...cartItem}
-                    className={styles.product}
-                    key={cartItem.id}
-                    onIncrement={() => incrementItem(cartItem)}
-                    onDecrement={() => decrementItem(cartItem)}
-                  />
-                ))}
-              </ul>
+              {!!cartItems.length ? (
+                <>
+                  <div className={cx(styles.products, styles.section)}>
+                    <h2 className={styles.heading}>Shopping Bag</h2>
+                    <ul className={styles.productList}>
+                      {cartItems.map(cartItem => (
+                        <Product
+                          {...cartItem}
+                          className={styles.product}
+                          key={cartItem.id}
+                          onIncrement={() => incrementItem(cartItem)}
+                          onDecrement={() => decrementItem(cartItem)}
+                        />
+                      ))}
+                    </ul>
+                  </div>
+                  <div className={cx(styles.summary, styles.section)}>
+                    <h2 className={styles.heading}>Order Summary</h2>
+                    <div>
+                      <div className={styles.summaryRow}>
+                        <span className={styles.summaryItem}>Subtotal</span>
+                        <span
+                          className={cx(
+                            styles.summaryItem,
+                            styles.summaryPrice
+                          )}
+                        >
+                          $0
+                        </span>
+                      </div>
+                      <div className={styles.summaryRow}>
+                        <span className={styles.summaryItem}>Taxes</span>
+                        <span
+                          className={cx(
+                            styles.summaryItem,
+                            styles.summaryPrice
+                          )}
+                        >
+                          $0
+                        </span>
+                      </div>
+                      <div className={styles.summaryRow}>
+                        <span className={styles.summaryItem}>Shipping</span>
+                        <span
+                          className={cx(
+                            styles.summaryItem,
+                            styles.summaryPrice
+                          )}
+                        >
+                          Free
+                        </span>
+                      </div>
+                      <div className={styles.summaryRow}>
+                        <span className={styles.summaryItem}>Total</span>
+                        <span
+                          className={cx(
+                            styles.summaryItem,
+                            styles.summaryPrice,
+                            styles.summaryItemBold
+                          )}
+                        >
+                          $0
+                        </span>
+                      </div>
+                    </div>
+                    <Button className={styles.checkoutBtn} onClick={checkout}>
+                      Checkout
+                    </Button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <img className={styles.emptyImage} src={empty} alt="empty" />
+                  <p className={cx(styles.text, styles.emptyTitle)}>
+                    Your bag is empty
+                  </p>
+                  <p className={cx(styles.text, styles.emptyText)}>
+                    Please add some products to your cart
+                  </p>
+                </>
+              )}
             </div>
-            <div className={cx(styles.summary, styles.section)}>
-              <h2 className={styles.heading}>Order Summary</h2>
-              <div>
-                <div className={styles.summaryRow}>
-                  <span className={styles.summaryItem}>Subtotal</span>
-                  <span className={cx(styles.summaryItem, styles.summaryPrice)}>
-                    $0
-                  </span>
-                </div>
-                <div className={styles.summaryRow}>
-                  <span className={styles.summaryItem}>Taxes</span>
-                  <span className={cx(styles.summaryItem, styles.summaryPrice)}>
-                    $0
-                  </span>
-                </div>
-                <div className={styles.summaryRow}>
-                  <span className={styles.summaryItem}>Shipping</span>
-                  <span className={cx(styles.summaryItem, styles.summaryPrice)}>
-                    Free
-                  </span>
-                </div>
-                <div className={styles.summaryRow}>
-                  <span className={styles.summaryItem}>Total</span>
-                  <span
-                    className={cx(
-                      styles.summaryItem,
-                      styles.summaryPrice,
-                      styles.summaryItemBold
-                    )}
-                  >
-                    $0
-                  </span>
-                </div>
-              </div>
-              <Button className={styles.checkoutBtn} onClick={checkout}>
-                Checkout
-              </Button>
-            </div>
-          </>
-        ) : (
-          <>
-            <img className={styles.emptyImage} src={empty} alt="empty" />
-            <p className={cx(styles.text, styles.emptyTitle)}>
-              Your bag is empty
-            </p>
-            <p className={cx(styles.text, styles.emptyText)}>
-              Please add some products to your cart
-            </p>
-          </>
-        )}
-      </div>
-    </div>
+          </div>
+        )
+      }}
+    </BrowserDetection>
   );
 }
 
