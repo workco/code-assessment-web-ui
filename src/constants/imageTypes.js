1b637d2dc941a9831cb605f0a12ea3f134d04945
Sarah Mogin <mogin@work.co>
2020-01-14 13:06:09 -0500

    fix: GraphQL-friendly image types

--- src/constants/imageTypes.js
+++ src/constants/imageTypes.js
@@ -1,10 +1,10 @@
 export default {
-  DEFAULT_RT: 'default-rt',
-  DEFAULT_SQ: 'default-sq',
-  ALT_1_RT: 'alt-1-rt',
-  ALT_1_SQ: 'alt-1-sq',
-  ALT_2_RT: 'alt-2-rt',
-  ALT_2_SQ: 'alt-2-sq',
-  ALT_3_RT: 'alt-3-rt',
-  ALT_3_SQ: 'alt-3-sq'
+  DEFAULT_RT: 'defaultRect',
+  DEFAULT_SQ: 'defaultSquare',
+  ALT_1_RT: 'alt1Rect',
+  ALT_1_SQ: 'alt1Square',
+  ALT_2_RT: 'alt2Rect',
+  ALT_2_SQ: 'alt2Square',
+  ALT_3_RT: 'alt3Rect',
+  ALT_3_SQ: 'alt3Square'
 };
