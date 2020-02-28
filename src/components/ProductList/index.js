a63aa51139347bf0eacf8217a96a249597ad4cd1
Estevão Lucas <lucas@work.co>
2020-02-28 18:06:38 -0500

    refactor: Create `ProductList` component to be used by `ProductLanding` page component

---
+++ src/components/ProductList/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Product from '../Product';
+
+import styles from './ProductList.module.scss';
+
+function ProductList({ addItem, products, cartItems }) {
+  const renderProduct = (product, isFeatured) => (
+    <Product
+      {...product}
+      isAdded={cartItems.some(({ id }) => id === product.id)}
+      onClick={() => addItem(product)}
+      key={product.id}
+      isFeatured={isFeatured}
+    />
+  );
+
+  return (
+    <div className={styles.products}>
+      {products[0] && renderProduct(products[0], true)}
+      <div className={styles.thumbnails}>
+        {[...products].slice(1).map(p => renderProduct(p))}
+      </div>
+    </div>
+  );
+}
+
+ProductList.propTypes = {
+  addItem: PropTypes.func,
+  cartItems: PropTypes.object,
+  products: PropTypes.object
+};
+
+export default ProductList;
--- src/pages/ProductLanding/index.js
+++ src/pages/ProductLanding/index.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
 
 import AppContext from '../../contexts/AppContext';
-import Product from '../../components/Product';
 import CartButton from '../../components/CartButton';
+import ProductList from '../../components/ProductList';
 
 import logo from '../../assets/logo.svg';
 
@@ -12,16 +12,6 @@ function ProductLanding() {
   const { addItem, products, cartItems } = useContext(AppContext);
 
   const cartQuantity = cartItems.reduce((acc, item) => acc + item.count, 0);
-
-  const renderProduct = (product, isFeatured) => (
-    <Product
-      {...product}
-      isAdded={cartItems.some(({ id }) => id === product.id)}
-      onClick={() => addItem(product)}
-      key={product.id}
-      isFeatured={isFeatured}
-    />
-  );
 
   return (
     <main className={styles.wrapper}>
@@ -34,12 +24,11 @@ function ProductLanding() {
         <img src={logo} alt="Daily deals" />
       </h1>
 
-      <div className={styles.products}>
-        {products[0] && renderProduct(products[0], true)}
-        <div className={styles.thumbnails}>
-          {[...products].slice(1).map(p => renderProduct(p))}
-        </div>
-      </div>
+      <ProductList
+        addItem={addItem}
+        cartItems={cartItems}
+        products={products}
+      />
     </main>
   );
 }
